0addd4fff5469bc636515cd04e0d983558b3bb0f
sblamb <sblamb@csc.cs.ucalgary.ca>
2015-03-24 13:32:12 -0600

    MenuItem class and style changes

--- CPSC481/Denu/Denu/MainWindow.xaml.cs
+++ CPSC481/Denu/Denu/MainWindow.xaml.cs
@@ -20,9 +20,16 @@ namespace Denu
     /// </summary>
     public partial class MainWindow : Window
     {
+        Dictionary<String, MenuItem> menu;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //Create menu items yo
+            menu = new Dictionary<String, MenuItem>();
+            menu.Add("Rum & Coke", new MenuItem("Rum & Coke", 6.99, new BitmapImage(new Uri("FoodDrink/RumNCoke.jpg", UriKind.Relative))));
+            menu.Add("Gin & Tonic", new MenuItem("Gin & Tonic", 6.99, new BitmapImage(new Uri("FoodDrink/gin-and-tonic.jpg", UriKind.Relative))));
         }
 
         private void begin(object sender, RoutedEventArgs e)
@@ -80,7 +87,7 @@ namespace Denu
 
         private void confirmAll(object sender, RoutedEventArgs e)
         {
-            sureTxt.Text = "Has everyone placed their order?";
+            sureTxt.Text = "Is everyone ready to place their order?";
             confirmGrd.Visibility = Visibility.Visible;
             dialogGrid.Visibility = Visibility.Visible;
         }
@@ -95,7 +102,6 @@ namespace Denu
         {
             itemImg.Source = new BitmapImage(new Uri("FoodDrink/RumNCoke.jpg", UriKind.Relative));
             priceLbl.Content = "Price: $7.99";
-            qtyPriceLbl.Content = "Total: $7.99";
             itemLbl.Content = "Rum & Coke";
         }
 
@@ -103,14 +109,16 @@ namespace Denu
         {
             itemImg.Source = new BitmapImage(new Uri("FoodDrink/gin-and-tonic.jpg", UriKind.Relative));
             priceLbl.Content = "Price: $7.99";
-            qtyPriceLbl.Content = "Total: $7.99";
             itemLbl.Content = "Gin & Tonic";
         }
 
         private void addItem(object sender, RoutedEventArgs e)
         {
             if(!itemLbl.Content.Equals("")){
+                billGrd.Visibility = Visibility.Visible;
                 pendItem1Grd.Visibility = Visibility.Visible;
+                pendImg1.Visibility = Visibility.Hidden;
+                checkImg1.Visibility = Visibility.Visible;
             }
         }
 
@@ -130,9 +138,20 @@ namespace Denu
         {
             confirmGrd.Visibility = Visibility.Hidden;
             dialogGrid.Visibility = Visibility.Hidden;
+            okBtn.Visibility = Visibility.Hidden;
+            yesBtn.Visibility = Visibility.Visible;
+            noBtn.Visibility = Visibility.Visible;
+
             foodGrd.Visibility = Visibility.Hidden;
-            drinkGrd.Visibility = Visibility.Hidden;
+            drinkGrd.Visibility = Visibility.Visible;
+
+            billGrd.Visibility = Visibility.Hidden;
             welcomeGrd.Visibility = Visibility.Visible;
+
+            itemImg.Source = null;
+            priceLbl.Content = "";
+            itemLbl.Content = "";
+            descTxt.Text = "";
         }
     }
 }
---
+++ CPSC481/Denu/Denu/MenuItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Denu
+{
+    class MenuItem
+    {
+        private String name;
+        private double price;
+        private BitmapImage image;
+
+        public MenuItem(String name, double price, BitmapImage image)
+        {
+            this.name = name;
+            this.price = price;
+            this.image = image;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPriceString()
+        {
+            return "Price: " + price.ToString();
+        }
+
+        public BitmapImage getImage()
+        {
+            return image;
+        }
+    }
+}
